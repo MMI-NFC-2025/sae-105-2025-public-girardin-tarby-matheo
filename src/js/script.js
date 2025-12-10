a58b827ea8640db00d42c940b3b4e60f92125203
girardin-tarby-matheo <matheo.girardin--tarby@edu.univ-fcomte.fr>
2025-12-10 16:39:45 +0100

    menu2

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,14 @@ const countdown = setInterval(() => {
     const now = new Date().getTime();
     const distance = targetDate - now;
 
-    const h12 = document.querySelector(".timer");
+    const h2 = document.querySelector(".timer");
 
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    h12.innerHTML = `${days}j ${hours}h ${minutes}m ${seconds}s` ;
+    h2.innerHTML = `${days}j ${hours}h ${minutes}m ${seconds}s` ;
 
     if (distance < 0) {
         clearInterval(countdown);
@@ -23,13 +23,16 @@ const toggle = document.getElementById('menuButton') || document.querySelector('
 const closeBtn = document.getElementById('menuClose');
 const nav = document.getElementById('mainNav') || document.querySelector('.menu');
 const page = document.body;
+const header = document.querySelector('.header')
 
 toggle.addEventListener('click', () => {
+    header.classList.toggle('close');
     nav.classList.toggle('open');
     page.classList.toggle('no-scroll');
 });
 
 closeBtn.addEventListener('click', () => {
+    header.classList.toggle('close');
     nav.classList.toggle('open');
     page.classList.toggle('no-scroll');
 });
