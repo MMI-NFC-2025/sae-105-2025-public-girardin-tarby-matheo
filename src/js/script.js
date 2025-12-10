6c154478870d670d74c5c3d4057c425642d69f9e
girardin-tarby-matheo <matheo.girardin--tarby@edu.univ-fcomte.fr>
2025-12-10 14:51:30 +0100

    menu

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,18 @@ const countdown = setInterval(() => {
         document.querySelector(".timer").textContent = "C'est maintenant 🎉";
     }
 }, 1000);
+
+const toggle = document.getElementById('menuButton') || document.querySelector('.menu-button');
+const closeBtn = document.getElementById('menuClose');
+const nav = document.getElementById('mainNav') || document.querySelector('.menu');
+const page = document.body;
+
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    page.classList.toggle('no-scroll');
+});
+
+closeBtn.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    page.classList.toggle('no-scroll');
+});
